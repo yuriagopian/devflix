628a261f4d6612f6fc98037a685b05593c31f9a4
yuri agopian <yuri.agopian@hotmail.com>
2020-07-28 00:18:17 -0300

    first commit

---
+++ front-end/src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Menu.css';
+import Button from '../Button';
+
+import Logo from '../../assets/img/Logo.png';
+
+function Menu () {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="DevFlix" />
+      </a>
+
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
