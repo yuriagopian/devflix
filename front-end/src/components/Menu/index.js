2b8527aa4b7d9ffa34263e1da576a21ac817321e
yuri <yuri.agopian@hotmail.com>
2020-07-29 01:17:17 -0300

    cadastro de video e de categoria

--- front-end/src/components/Menu/index.js
+++ front-end/src/components/Menu/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './Menu.css';
 import Button from '../Button';
 
 import Logo from '../../assets/img/Logo.png';
 
-function Menu () {
-  return (  
+function Menu() {
+  return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="DevFlix" />
-      </a>
+      </Link >
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
