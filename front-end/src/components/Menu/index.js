d8894e64d9368e629ece9a5dc7fa572958f409a5
yuri <yuri.agopian@hotmail.com>
2020-07-29 01:37:09 -0300

    adicionando carrousel

--- front-end/src/components/Menu/index.js
+++ front-end/src/components/Menu/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-
-import Logo from '../../assets/img/Logo.png';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
   return (
     <nav className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt="DevFlix" />
-      </Link >
+        <img className="Logo" src={Logo} alt="DevFlix logo" />
+      </Link>
 
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-import Menu from '../../components/Menu';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
-import dadosIniciais from '../../data/dados_iniciais.json';
 
-function Home () {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu />
@@ -28,19 +27,19 @@ function Home () {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />
+      />      
 
       <Carousel
         category={dadosIniciais.categorias[3]}
-      />
+      />      
 
       <Carousel
         category={dadosIniciais.categorias[4]}
-      />
+      />      
 
       <Carousel
         category={dadosIniciais.categorias[5]}
-      />
+      />      
 
       <Footer />
     </div>
