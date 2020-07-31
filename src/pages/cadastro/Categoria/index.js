e23fcf4cfe9a49860122e70738f6f0c64ac8eac3
yuri <yuri.agopian@hotmail.com>
2020-07-31 03:32:02 -0300

    ajuste na url

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -43,7 +43,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://devfliix.vercel.app/cadastro/categoria';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
